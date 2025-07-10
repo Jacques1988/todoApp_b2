e0b65663860007fb0a0bff20e4f48fda93004bfc
Jacques <jacquesvanluyck@gmail.com>
2025-07-10 09:30:08 +0200

    we generated a new task component with responsive desing globally and component based, fixes #15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { FilterComponent } from "./filter/filter.component";
-
+import { HeaderComponent } from './header/header.component';
+import { FilterComponent } from './filter/filter.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FilterComponent],
+  imports: [HeaderComponent, FilterComponent, NewTaskComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoApp';
