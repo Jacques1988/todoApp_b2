c272acdc4e42985222d58c100841599b172e9b3a
Jacques <jacquesvanluyck@gmail.com>
2025-07-11 11:03:21 +0200

    update interface for tasks and build task dummy data

---
+++ src/app/shared/tasks.ts
@@ -0,0 +1,27 @@
+[
+    {
+        id: "1",
+        task: "Dackel füttern",
+        status: "complete"
+    },
+    {
+        id: "2",
+        task: "Dackel gießen",
+        status: "incomplete",
+    },
+    {
+        id: "3",
+        task: "Dackel knuddeln",
+        status: "complete",
+    },
+    {
+        id: "4",
+        task: "Einkaufen",
+        status: "incomplete",
+    },
+    {
+        id: "5",
+        task: "Spazieren",
+        status: "complete",
+    }
+]
